a152896d7ec01b72432d27d0bb4aa1933e020b9a
UlugKh <141512061+UlugKh@users.noreply.github.com>
2024-09-19 22:54:38 +0500

    Update Homework1.java

--- student/Homework1.java
+++ student/Homework1.java
@@ -8,28 +8,41 @@ public class Homework1 {
         } else if (n == 1) {
             return 10.95;
         } else if (n>1) {
-            return 10.95 + n * 2.95;
+            return 10.95 + (n-1) * 2.95;
         }
         
         return 0;
     }
     public boolean isValidTriangle(int a, int b, int c){
-        //TODO Write your code here for problem 2
+        return a + b > c && a + c > b && b + c > a;
         return false;
 
     }
     public boolean isPrime(int number){
-        //TODO Write your code here for problem 3
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
         return false;
     }
 
     public  int hex2int(String hex) {
-        //TODO Write your code here for problem 4
-        return 0;
+        hex = hex.toUpperCase();
+        if (hex.length() != 1 || !("0123456789ABCDEF".contains(hex))) {
+            return -1;
+        }
+        return Integer.parseInt(hex, 16);
     }
     public String int2hex(int value) {
-        //TODO Write your code here for problem 4
-        return "";
+        if (value < 0 || value > 15) {
+            return "-1";
+        }
+        return Integer.toHexString(value).toUpperCase();
     }
 
 
